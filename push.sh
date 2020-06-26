0ffa6f1b19e380fd722553d19c2c4aeae0019554
michael KAMM <mawuenamichelk@gmail.com>
2020-06-26 12:19:45 +0000

    Update

--- push.sh
+++ push.sh
@@ -9,7 +9,7 @@
 
 #git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'Update'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
